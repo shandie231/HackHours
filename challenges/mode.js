2fa0c1e9a41632abc2dd58ce3e72ee19f4ccbc93
Michael Shand <shandie231@gmail.com>
2023-07-25 10:02:58 -0400

    add mode challenge

--- challenges/mode.js
+++ challenges/mode.js
@@ -12,8 +12,39 @@ mode([7, 5, 8, 8, 2, 5]) -> 8
 */
 
 const mode = array => {
-  
+    //declare empty object to hold numbers
+    let numHolder = {};
+    let outputNum = 0;
+    //iterate through array setting the array values to the keys and the number of each number as the value
+    if(array.length === 1){
+        return array[0];
+    }
+    for(let i = 0; i<array.length; i++){
+        if(!numHolder[array[i]]){
+            numHolder[array[i]] = 0;
+        }
+        numHolder[array[i]] = numHolder[array[i]]+ 1;
+    }
+    const keys = Object.keys(numHolder);
+    const values = Object.values(numHolder);
+   
+    
+    for(let j = 1; j<keys.length; j++){
+       
+        if(numHolder[keys[j]] === numHolder[outputNum]){
+        outputNum = keys[j];
+        }
+        else if(numHolder[keys[j]] > outputNum){
+            outputNum = keys[j];
+        }
+    
+    }
+    return outputNum;
 };
+console.log(mode([3, 2, 4, 3])) //-> 3
+console.log(mode([7, 5, 8, 8, 2, 5]))// -> 8
+console.log(mode([]))// -> 8
+console.log(mode([1,1,1,1]))// -> 8
 
 /*
 
