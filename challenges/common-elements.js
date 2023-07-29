267e126a8d2a604c163ebbeafc7a622fb23f07ca
Michael Shand <shandie231@gmail.com>
2023-07-29 10:04:27 -0400

    hackhour challenge

--- challenges/common-elements.js
+++ challenges/common-elements.js
@@ -15,9 +15,44 @@ arr3 = [2, 100, 2000, 'dog', 3, 'lion'];
 commonElements(arr1, arr2, arr3) -> [2, 3, 2000, 'dog']
 */
 
+const { parseArgs } = require("util");
+
 const commonElements = (...args) => {
 
+    const dupVals = [];
+    const obj = {};
+    const outputArray = [];
+    const newArray = [...args];
+    
+    // console.log(newArray);
+    // const map1 = newArray.map((x) => outputArray.push(x));
+    // console.log(map1);
+    
+    
+    console.log(newArray);
+    for(let i =0; i<newArray.length; i++){
+        let currArr = newArray[i];
+        console.log(currArr);
+        for(let j = 0; j<currArr.length; j++){
+            outputArray.push(currArr[j]);
+            if(!obj[currArr[j]]){
+                obj[currArr[j]] = 1; 
+            }else{
+                dupVals.push(currArr[j]);
+            }
+        }
+        
+    }
+    console.log(dupVals);
+
+
 }
+
+
+arr1 = [2, 10,'cat', 3, 99, 2000, 'dog', 'lion'];
+arr2 = [3, 7, 2, 2000, 1, 'dog', 'cat'];
+arr3 = [2, 100, 2000, 'dog', 3, 'lion'];
+console.log(commonElements(arr1, arr2, arr3));
 
 
 
