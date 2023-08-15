69e09e3be93867e40cffc186a48bbe213c47b824
Michael Shand <shandie231@gmail.com>
2023-08-15 09:57:06 -0400

    reverse array

--- challenges/reverse-array.js
+++ challenges/reverse-array.js
@@ -7,8 +7,24 @@ to the array prototype while solving the problem.
 */
 
 const reverseArray = array => {
+  let length = (array.length * 2) - 1;
+  const ogLength = array.length;
+  for(let i = 0; i<ogLength; i++){
+    array[length] = array[0];
+    array.splice(0,1);
+    length = length - 2;
+  };
+  
+
+
+  return array;
+
   
 };
+const array1 = [1,2,3,4,5];
+const array2 = [5,3,1,4,2];
+console.log(reverseArray(array1));
+console.log(reverseArray(array2));
 
 /*
 
@@ -25,6 +41,13 @@ input string will always have at least one word
 */
 
 const reverseSentence = sentence => {
+  let start = 0;
+  let end = 0;
+  
+  for(let i = 0; i<sentence.length; i++){
+  
+    
+  }
   
 };
 
