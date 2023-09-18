303d0292395b35344494b32d918471649524488b
Michael Shand <shandie231@gmail.com>
2023-09-18 10:07:19 -0400

    needle-haystack complete no extension

--- challenges/needle-haystack.js
+++ challenges/needle-haystack.js
@@ -17,7 +17,23 @@ Output: false
 
 const needleInHaystack = (string, substring) => {
 
+  if(!string.length) return false;
+  let subStringVal = substring[0];
+  let stringVal = string[0]
+
+  if(stringVal === subStringVal){
+    let temp = string;
+    let sliceBit = temp.slice(0, substring.length);
+    if(sliceBit === substring) return true;
+    else return needleInHaystack(string.slice(1), substring)
+  }else{
+    return needleInHaystack(string.slice(1), substring);
+  }
+
+
 };
+
+console.log(needleInHaystack('xztzcatbfbbq', 'cat'));
 
 /*
 Extension: Now imagine the target substring and string both might have underscores '_'.
@@ -31,7 +47,27 @@ Output: false
 */
 
 const needleInHaystackWithWildcards = (string, substring) => {
+  if(!string.length) return false;
+  let subStringVal = substring[0];
+  let stringVal = string[0]
 
+  // if(stringVal === subStringVal || "_"){
+  //   let temp = string;
+  //   let subTemp = substring;
+  //   let sliceBit = temp.slice(0, substring.length);
+  //   function helper(stringTemp, substringTemp){
+  //     if(stringTemp === substringTemp) return true;
+  //       if(stringTemp[0] === "_" || substringTemp[0] === "_"){
+  //        return helper(stringTemp.slice(1), substringTemp.slice(1));
+  //       }
+      
+  //   }
+    
+  //   helper(sliceBit, subTemp);
+  //   return needleInHaystackWithWildcards(string.slice(1), substring)
+  // }
 };
+
+console.log(needleInHaystackWithWildcards('_ello_orld', 'helloworl_'))
 
 module.exports = { needleInHaystack, needleInHaystackWithWildcards };
