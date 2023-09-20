199101f372c80fed92e1977ab870d38082b35168
Michael Shand <shandie231@gmail.com>
2023-09-20 10:05:33 -0400

    generate parens complete

--- challenges/generate-parentheses.js
+++ challenges/generate-parentheses.js
@@ -30,10 +30,25 @@ Given n = 0, a solution set is:
 
 const generateParentheses = n => {
   
-  
+  const result = [];
 
+  breathFirstSearch('', 0, 0);
+  return result;
+
+  function breathFirstSearch(str, left, right){
+    if(left === n && right === n){
+      result.push(str);
+      return;
+    }
+    if(left !== n){
+      breathFirstSearch(str + '(', left + 1, right);
+    }
+    if(left > right){
+      breathFirstSearch(str + ')', left, right + 1);
+    }
+  }
 };
 
-console.log(generateParentheses(2));
+console.log(generateParentheses(1));
 
 module.exports = {generateParentheses};
