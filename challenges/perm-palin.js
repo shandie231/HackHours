700a922d00e098816bb3af15fb3683e856644a16
Michael Shand <shandie231@gmail.com>
2023-09-25 10:00:21 -0400

    perm-palin not complete

--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -15,8 +15,43 @@
  */
 
 const permPalin = str => {
+  console.log(str.length);
+  console.log(str);
+  if(str.length === 1) return true;
+
+  let start = 0;
+  let end = str.length -1;
+  let mid = Math.floor(end/2);
+
+  let firstHalf = str.slice(start, mid + 1);
+  let secondHalf = str.slice(mid + 1, end + 1);
+  let flipSecond = '';
+  console.log(firstHalf);
+  console.log(secondHalf);
+
+
+
+  for(let i = secondHalf.length - 1; i >= 0; i--){
+    flipSecond = flipSecond.concat(secondHalf[i]);
+  }
+
+  console.log(flipSecond);
+
+
+  if(firstHalf === flipSecond || firstHalf === secondHalf) return true;
+  if(str.length === 2) return false;
+  else{
+    return permPalin(str.slice((str.length - 1) * -1));
+  }
+
 
 };
+
+console.log(permPalin('abab'));
+console.log(permPalin('cabab'));
+console.log(permPalin('cabab'));
+console.log(permPalin('cbac'));
+console.log(permPalin('a'));
 
 /* 
  * Extension: Solve in constant space complexity.
