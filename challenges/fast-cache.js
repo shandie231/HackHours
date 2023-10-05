50b4f3a979d4c4080b7bc9a7a1f39aa8532ae8d0
Michael Shand <shandie231@gmail.com>
2023-10-05 14:43:49 -0400

    practice cacheing algos

--- challenges/fast-cache.js
+++ challenges/fast-cache.js
@@ -2,7 +2,7 @@
  define a function "fastCache" that takes one argument, a function, and returns a function. 
  When fastCache is invoked it creates an object that tracks calls to the returned function, where each input to the returned function is associated with its output. 
  Every subsequent call to that returned function with the same argument will return the output directly from the object, instead of invoking the original function again.
-*/
+sdfsd*/
 
 const fastCache = func => {
     ///create an empty object
