bcbc3a4f9f36719a613d3e763e143fa2b4870a69
Michael Shand <shandie231@gmail.com>
2023-10-24 17:50:50 -0400

    More algos, working on two-sum and three-sum, all tests passed

--- challenges/max-subarray.js
+++ challenges/max-subarray.js
@@ -90,71 +90,34 @@ console.log(maxSubarray(array6));
 */
 
 const kadanesMaxSubarray = (nums) => {
-  if(!Array.isArray(nums)) return undefined;
-  let start = 0;
-  let pointer = start;
-  let end = start;
-  let total = 0;
-  let maxTotal = nums[start];
-  let outputArr = nums.slice(start, start + 1);
-  for(let i = start; i < nums.length + 1; i++){
-    //check the start value and make sure it is not a negative. - If it is a negative, look at the next value, if the next value is greater than the current index set start to the next value
-    if(Math.sign(nums[start]) === -1){
-      //check if the next index is greater than the current
-      if(nums[i] < nums[i+1]){
-        //set start to the next index
-        start = i + 1;
-        pointer = start + 1;
-        total = nums[start];
-        maxTotal = total;
-        
-        //set single index to output array
-        outputArr = nums.slice(start, pointer);
-      }
-    }else{
-      //conditional to check for
-      //* If the next index is negetive 
-      //*   AND if the next index is less than the current
-      //* OR if at the last index in the array
-      console.log(total);
-      console.log(nums[pointer])
-      if(((Math.sign(nums[pointer]) === -1) && (total <= Math.abs(nums[pointer]))) || nums[pointer] === undefined){
-        //this conditional will set the output array, and reset the pointers and total
-        end = i;
-        //check if the current total is greater than the cached total
-        if(total > maxTotal){
-          maxTotal = total;
-          outputArr = nums.slice(start, end);
-        }
-        start = pointer + 1;
-        i = start;
-        total = nums[start];
-        pointer = start + 1;
-      }else{ 
-        //if that complex conditional fails, add the next index to the total and increment the pointer 
-        total += nums[pointer];
-        pointer++;
-      }
-    }
+  
+  if(nums.length === 0) return undefined;
+
+  let currentSum = nums[0];
+  let maxSum = nums[0];
+
+  for(let i = 1; i < nums.length; i++){
+    currentSum = Math.max(nums[i], currentSum + nums[i]);
+    maxSum = Math.max(currentSum, maxSum);
   }
-  return outputArr;
+  return maxSum;
+
 }
 
-// const array1 = [-2,1,-3,4,-1,2,1,-5,4];
-// const array2 = [1];
-// const array3 = [5,4,-1,7,8];
-// const array4 = [-100, -20, -5, -10];
-// const array5 = [1,2,-1,-2,2,1,-2,1,4,-5,4];
-// const array6 = [1, -2, 3, 10, -4, 7, 2, -5]
+const karray1 = [-2,1,-3,4,-1,2,1,-5,4];
+// const karray2 = [1];
+// const karray3 = [5,4,-1,7,8];
+// const karray4 = [-100, -20, -5, -10];
+// const karray5 = [1,2,-1,-2,2,1,-2,1,4,-5,4];
+// const karray6 = [1, -2, 3, 10, -4, 7, 2, -5]
 
-console.log(maxSubarray(array1));
-console.log(maxSubarray(array2));
-console.log(maxSubarray(array3));
-console.log(maxSubarray(array4));
-console.log(maxSubarray(array5));
-console.log(maxSubarray(array6));
+console.log(kadanesMaxSubarray(karray1));
+// console.log(kadanesMaxSubarray(karray2));
+// console.log(kadanesMaxSubarray(karray3));
+// console.log(kadanesMaxSubarray(karray4));
+// console.log(kadanesMaxSubarray(karray5));
+// console.log(kadanesMaxSubarray(karray6));
 
-console.log(kadanesMaxSubarray(array1));
 // console.log(kadanesMaxSubarray(array2));
 // console.log(kadanesMaxSubarray(array3));
 // console.log(kadanesMaxSubarray(array4));
--- challenges/two-sum.js
+++ challenges/two-sum.js
@@ -18,25 +18,19 @@
 
 
 const twoSum = (arr, target) => { 
-  const targetArray = [];
-  const outObj = {};
+  if(arr.length === 0) return underfined;
+  const cache = new Map();
 
-  for(let i = 0; i<arr.length; i++){
-    targetArray.push(target - arr[i]);
-
-    if(!outObj[targetArray[i]]) outObj[targetArray[i]] = 1;
-    else outObj[targetArray[i]]++;
-
-    if(!outObj[arr[i]]) outObj[arr[i]] = 1;
-    else outObj[arr[i]]++;
-
-    if(outObj[arr[i]] >= 2 || outObj[targetArray[i]] >= 2) return true;
-
-  
+  for(let i = 0; i < arr.length; i++){
+    if(cache.has(arr[i])) return [cache.get(arr[i]), i];
+    cache.set(target - arr[i], i);
   }
   return false;
-
 }
+
+console.log(twoSum([2,7,11,15], 9));
+console.log(twoSum([3,2,4], 6));
+console.log(twoSum([3,3], 6));
 
 
 const nums = [2, 5, 11, 15]
@@ -49,6 +43,23 @@ console.log(twoSum(nums2, 7));
 console.log(twoSum(nums, 9))// -> false
 
 /*
+
+  Given an array of numbers and a target number,
+  return true if two of the numbers in the array add up to the target.
+  Otherwise, return false.
+
+  You may assume that each input would have exactly one solution, and you may not use the same element twice.
+  The straightforward way to solve this problem would take O(n²)time. Is it possible to do this in O(n) time? 
+
+  Example:
+
+  const nums = [2, 5, 11, 15]
+  twoSum(num, 7) -> true
+  Rational:  nums[0] + nums[1] = 2 + 5 = 7,
+
+  twoSum(nums, 9) -> false
+  Rational: No elements inside the array sum up to the target number
+
 Extension:
 Given an array of numbers and a target number, 
 return true if three of the numbers in the array add up to the target.
@@ -60,7 +71,28 @@ The straightforward way to solve this problem would take O(n³) time. Is it poss
 
 const threeSum = (arr, target) => { 
 
+  const cache = new Map();
+
+  for(let i = 0; i < arr.length; i++){
+ 
+    for(let j = 1; j < arr.length; j++){
+      if(cache.has(arr[i])){
+        let cacheVal = cache.get(arr[i]);
+        if(!cacheVal.includes(arr[i])) return true;
+      }
+      cache.set(target - (arr[i] + arr[j]), [arr[i], arr[j]]);
+      console.log(cache);
+    }
+  }
+  return false;
 }
+let newArr = [2, 5, 11, 15];
+let newArr2 = [1, 4, 7, 2, 9, 0];
+let newTarget  = 18;
+let newTarget2  = 45;
+//console.log(threeSum(newArr, newTarget));
+console.log(threeSum([1, 4, 7, 2, 9, 0], 19));
+//console.log(threeSum([1, 4, 6, 12, 9], 45));
 
 
 
