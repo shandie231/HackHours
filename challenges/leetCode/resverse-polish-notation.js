41c27d05f0a7de3d7b55b64d92d0ba3b5ce7ded8
Michael Shand <shandie231@gmail.com>
2023-11-07 09:24:12 -0500

    hackhour Reverse Polish Notation

---
+++ challenges/leetCode/resverse-polish-notation.js
@@ -0,0 +1,35 @@
+/*
+You are given an array of strings tokens that represents an arithmetic expression in a Reverse Polish Notation.
+
+Evaluate the expression. Return an integer that represents the value of the expression.
+ 
+
+Example 1:
+
+Input: tokens = ["2","1","+","3","*"]
+Output: 9
+Explanation: ((2 + 1) * 3) = 9
+Example 2:
+
+Input: tokens = ["4","13","5","/","+"]
+Output: 6
+Explanation: (4 + (13 / 5)) = 6
+Example 3:
+
+Input: tokens = ["10","6","9","3","+","-11","*","/","*","17","+","5","+"]
+Output: 22
+Explanation: ((10 * (6 / ((9 + 3) * -11))) + 17) + 5
+= ((10 * (6 / (12 * -11))) + 17) + 5
+= ((10 * (6 / -132)) + 17) + 5
+= ((10 * 0) + 17) + 5
+= (0 + 17) + 5
+= 17 + 5
+= 22
+*/
+/**
+ * @param {string[]} tokens
+ * @return {number}
+ */
+const evalRPN = function(tokens){
+
+}
