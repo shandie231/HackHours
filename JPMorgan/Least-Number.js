52fb2abcd6671dbfec41deb39a2b58cdf78b786a
Michael Shand <shandie231@gmail.com>
2023-12-11 15:11:45 -0500

    least number complete

--- JPMorgan/Least-Number.js
+++ JPMorgan/Least-Number.js
@@ -4,5 +4,21 @@
  * @return {number}
  */
 const findLeastNumOfUniqueInts = function(arr, k) {
-    
+  let m = new Map();
+  arr.forEach(num => m.set(num, m.get(num)+1 || 1));
+  let occurances = Array.from(m.values());
+  occurances.sort((a,b) => a-b);
+  let res = occurances.length;
+  for (let num of occurances) {
+    if (k >= num) {
+      k -= num;
+      res--;
+    }
+    else return res;
+  }
+  return res;
+
+
 };
+
+console.log(findLeastNumOfUniqueInts([5,5,4], 1))
